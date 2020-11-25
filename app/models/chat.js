c396f86624713aeab97066c1642a7bfd7c59bf2b
ashivs2500 <antonioshivers2500@yahoo.com>
2020-11-25 17:40:45 -0500

    initial commit

---
+++ app/models/chat.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const chatSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Chat', chatSchema)
---
+++ curl-scripts/message/update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/chats"
+curl "${API}${URL_PATH}/${ID}" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+--header "Authorization: Bearer ${TOKEN}" \
+--data '{
+    "chat": {
+    "text": "'"${TEXT}"'"
+  }
+}'
+echo
