56579bfe4e49ba4916320fab55a13082d135cc3b
ajevans <ajecoder@gmail.com>
2020-12-01 16:35:29 -0500

    remove required tag from text key

--- app/models/chat.js
+++ app/models/chat.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const chatSchema = new mongoose.Schema({
   text: {
     type: String
+    // required: true
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
