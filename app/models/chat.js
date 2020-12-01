8acdf5e308ffb8d19e1f1cd4356fbffa41184ba8
mp4 <puleri.mp4@gmail.com>
2020-12-01 08:31:40 -0800

    Chat functional Chats can now be blank messages.

--- app/models/chat.js
+++ app/models/chat.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose')
 
 const chatSchema = new mongoose.Schema({
   text: {
-    type: String,
-    required: true
+    type: String
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
